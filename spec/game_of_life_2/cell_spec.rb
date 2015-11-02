813089992755ba6f7b1957c5e5a41335fdc5b165
bitfurry <sahilkharb@iitj.ac.in>
2015-11-02 11:44:16 +0530

    A live cell now speaks that I am not Dead

--- spec/game_of_life_2/cell_spec.rb
+++ spec/game_of_life_2/cell_spec.rb
@@ -8,6 +8,10 @@ module Universe
       it 'Live cell says I am alive' do
         expect(Cell.new(ALIVE).is_alive?).to eq(true)
       end
+
+      it 'Live cell says I am not dead' do
+        expect(Cell.new(ALIVE).is_alive?).not_to eq(false)
+      end
     end
   end
 end
