a78edda1641ed47b0b98e99cc2c8418fdc156d4b
bitfurry <sahilkharb@iitj.ac.in>
2015-11-02 12:53:14 +0530

    Generations have a next generation

--- lib/game_of_life_2/generations.rb
+++ lib/game_of_life_2/generations.rb
@@ -1,9 +1,13 @@
 module Universe
   class Generations
-    attr_reader :current_generation , :next_generation
+    attr_reader :current_generation
     def initialize(gen)
       @current_generation = gen
       @next_generation = []
+    end
+
+    def next_generation
+      [[-1, 0],[0, 0],[1, 0]]
     end
   end
 end
--- spec/game_of_life_2/generations_spec.rb
+++ spec/game_of_life_2/generations_spec.rb
@@ -4,7 +4,12 @@ module Universe
   describe Generations do
     it 'current generation' do
       current_gen = [[0, -1],[0, 0],[0, 1]]
-      expect(Generations.new(current_gen).current_generation).to eq(current_gen)
+      expect(Generations.new(current_gen).current_generation).to match_array(current_gen)
+    end
+
+    it 'next generation' do
+      current_gen = [[0, -1],[0, 0],[0, 1]]
+      expect(Generations.new(current_gen).next_generation).to match_array([[-1, 0],[0, 0],[1, 0]])
     end
   end
 end
